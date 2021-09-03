9c4822695cf5424c245394b0c76503e2626c23e2
hxtree <matthewheroux@gmail.com>
2021-09-03 00:48:50 -0500

    Add Outcome interface

---
+++ src/Contract/OutcomeInterface.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * This file is part of the LuckByDice package.
+ *
+ * (c) 2020-2021 Ouxsoft <contact@ouxsoft.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Ouxsoft\LuckByDice\Contract;
+
+interface OutcomeInterface
+{
+
+    public function __construct(array $dice);
+
+    public function getMin(): int;
+
+    public function setMin(int $min): void;
+
+    public function getMax(): int;
+
+    public function setMax(int $max): void;
+
+    public function setDice(Dice $die) : void;
+
+    public function getDice() : array;
+
+}
+
+
